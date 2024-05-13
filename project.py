dee5030d97b65a6329e0f74b4366930a6587e19a
ZhiltsovMax <106375787+ZhiltsovMax@users.noreply.github.com>
2024-05-13 19:59:39 +0300

    project.py
    v.2

--- project.py
+++ project.py
@@ -1,29 +1,36 @@
-class UserRegistration:
-    def __init__(self):
-        self.name = ""
-        self.surname = ""
-        self.birth_date = ""
-        self.gender = ""
-        self.email = ""
-        self.username = ""
-        self.password = ""
-        self.confirm_password = ""
+import re
 
-    def register_user(self):
-        self.name = input("Введите ваше имя: ")
-        self.surname = input("Введите вашу фамилию: ")
-        self.birth_date = input("Введите вашу дату рождения: ")
-        self.gender = input("Введите ваш пол: ")
-        self.email = input("Введите ваш E-mail: ")
-        self.username = input("Введите ваш логин: ")
-        self.password = input("Введите ваш пароль: ")
-        self.confirm_password = input("Повторите ваш пароль: ")
+def register():
+    name = input("Введите ваше имя (от 6 до 30 символов, используйте буквы латинского алфавита, цифры и точки): ")
+    while not re.match(r'^[a-zA-Z0-9.]{6,30}$', name):
+        name = input("Некорректное имя. Попробуйте снова: ")
 
-        if self.password != self.confirm_password:
-            print("Пароли не совпадают. Попробуйте снова.")
-            self.register_user()
-        else:
-            print("Регистрация прошла успешно!")
+    surname = input("Введите вашу фамилию (от 6 до 30 символов, используйте буквы латинского алфавита, цифры и точки): ")
+    while not re.match(r'^[a-zA-Z0-9.]{6,30}$', surname):
+        surname = input("Некорректная фамилия. Попробуйте снова: ")
 
-user = UserRegistration()
-user.register_user()
+    birth_date = input("Введите вашу дату рождения в формате дд-мм-гггг: ")
+    # Дополнительные проверки для возраста могут быть добавлены
+
+    gender = input("Введите ваш пол (мужской или женский): ")
+    while gender.lower() not in ['мужской', 'женский']:
+        gender = input("Некорректный пол. Введите мужской или женский: ")
+
+    city = input("Введите ваш город: ")
+
+    email = input("Введите ваш email: ")
+    while not re.match(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
+        email = input("Некорректный email. Попробуйте снова: ")
+
+    username = input("Введите ваш логин: ")
+
+    password = input("Введите ваш пароль: ")
+    confirm_password = input("Повторите ваш пароль: ")
+    while password != confirm_password:
+        print("Пароли не совпадают. Попробуйте снова.")
+        password = input("Введите ваш пароль: ")
+        confirm_password = input("Повторите ваш пароль: ")
+
+    print("Регистрация успешно выполнена!")
+
+register()
